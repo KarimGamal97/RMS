b1228b4223cc8d0e510d42cdab7953efaa018584
KarimGamal97 <kariimgamal1997@gmail.com>
2023-10-17 07:45:51 +0300

    change base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Broker_Project",
+  base: "/RMS",
   plugins: [vue()],
   resolve: {
     alias: {
